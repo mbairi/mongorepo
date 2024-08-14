0624db79dc82b6037ccee159871aea3ccd19fa90
Manjunatha Bairi <manjunatha.bairi@tabnerglobal.com>
2024-08-14 16:37:59 +0530

    package test

--- example/simplerepository.go
+++ example/simplerepository.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/mbairi/mongorepo"
+	mongorepo "github.com/mbairi/mongorepo/main"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
