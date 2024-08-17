c2689fa4b1e8f77dde79595c1a49a87c98c607f2
Manjunatha Bairi <manjunatha.bairi@tabnerglobal.com>
2024-08-17 10:30:16 +0530

    switch to builder style

---
+++ src/querybuilder.go
@@ -0,0 +1,145 @@
+package repo
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type QueryBuilder[T any] struct {
+	repo       *MongoRepository[T]
+	filter     string
+	projection string
+	sort       string
+	context    context.Context
+	pageable   [2]int
+}
+
+type Query struct {
+	filter     bson.M
+	projection bson.M
+	sort       bson.D
+	context    context.Context
+	pageable   [2]int
+}
+
+func (q *QueryBuilder[T]) Filter(filter string) *QueryBuilder[T] {
+	q.filter = filter
+	return q
+}
+
+func (q *QueryBuilder[T]) Projection(projection string) *QueryBuilder[T] {
+	q.projection = projection
+	return q
+}
+
+func (q *QueryBuilder[T]) Sort(sort string) *QueryBuilder[T] {
+	q.sort = sort
+	return q
+}
+
+func (q *QueryBuilder[T]) Pageable(page [2]int) *QueryBuilder[T] {
+	q.pageable = page
+	return q
+}
+
+func (q *QueryBuilder[T]) Context(ctx context.Context) *QueryBuilder[T] {
+	q.context = ctx
+	return q
+}
+
+func (q *QueryBuilder[T]) Count(params ...interface{}) (int64, error) {
+	query, err := q.ConstructQuery(params)
+	if err != nil {
+		return 0, err
+	}
+	return q.repo.Count(query)
+}
+
+func (q *QueryBuilder[T]) QueryOne(params ...interface{}) (T, error) {
+	query, err := q.ConstructQuery(params...)
+	if err != nil {
+		var result T
+		return result, err
+	}
+
+	return q.repo.QueryOne(query)
+}
+
+func (q *QueryBuilder[T]) QueryMany(params ...interface{}) ([]T, error) {
+	query, err := q.ConstructQuery(params)
+	if err != nil {
+		var result []T
+		return result, err
+	}
+	return q.repo.QueryMany(query)
+}
+
+func (q *QueryBuilder[T]) Delete(params ...interface{}) (int64, error) {
+	query, err := q.ConstructQuery(params)
+	if err != nil {
+		return -1, err
+	}
+	return q.repo.Delete(query)
+}
+
+func (q *QueryBuilder[T]) ConstructQuery(params ...interface{}) (Query, error) {
+	classic := Query{
+		pageable: q.pageable,
+		context:  q.context,
+	}
+
+	if q.filter != "" {
+		queryStr := q.replaceParams(q.filter, params...)
+		err := bson.UnmarshalExtJSON([]byte(queryStr), true, &classic.filter)
+		if err != nil {
+			return Query{}, fmt.Errorf("error parsing query: %w", err)
+		}
+	}
+
+	if q.projection != "" {
+		err := bson.UnmarshalExtJSON([]byte(q.projection), true, &classic.projection)
+		if err != nil {
+			return Query{}, fmt.Errorf("error parsing projection: %w", err)
+		}
+	}
+
+	if q.sort != "" {
+		var sortMap map[string]interface{}
+		err := json.Unmarshal([]byte(q.sort), &sortMap)
+		if err != nil {
+			return Query{}, fmt.Errorf("error parsing sort: %w", err)
+		}
+		for k, v := range sortMap {
+			classic.sort = append(classic.sort, bson.E{Key: k, Value: v})
+		}
+	}
+
+	return classic, nil
+}
+
+func (q *QueryBuilder[T]) replaceParams(query string, params ...interface{}) string {
+	for i, param := range params {
+		placeholder := fmt.Sprintf("?%d", i+1)
+		var replacement string
+
+		// Handle strings separately to avoid wrapping them in arrays
+		switch v := param.(type) {
+		case string:
+			replacement = fmt.Sprintf(`"%s"`, v)
+		default:
+			marshaledValue, err := json.Marshal(v)
+			if err != nil {
+				replacement = fmt.Sprintf("%v", v)
+			} else {
+				replacement = string(marshaledValue)
+			}
+		}
+
+		query = strings.Replace(query, placeholder, replacement, -1)
+	}
+	return query
+}
---
+++ src/repository.go
@@ -0,0 +1,328 @@
+package repo
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoRepository[T any] struct {
+	collection   *mongo.Collection
+	idFieldIndex int
+}
+
+func NewMongoRepository[T any](collection *mongo.Collection) (*MongoRepository[T], error) {
+
+	repo := &MongoRepository[T]{
+		collection: collection,
+	}
+
+	if err := repo.setIdField(); err != nil {
+		return nil, err
+	}
+	if err := repo.ensureSimpleIndexes(); err != nil {
+		return nil, err
+	}
+	if err := repo.ensureCompoundIndex(); err != nil {
+		return nil, err
+	}
+	return repo, nil
+}
+
+func (r *MongoRepository[T]) setIdField() error {
+	var dummy T
+	t := reflect.TypeOf(dummy)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	for i := 0; i < t.NumField(); i++ {
+		if tag := t.Field(i).Tag.Get("bson"); tag != "" {
+			tags := strings.Split(tag, ",")
+			for _, t := range tags {
+				if strings.TrimSpace(t) == "_id" {
+					r.idFieldIndex = i
+					return nil
+				}
+			}
+		}
+	}
+
+	return errors.New("type does not have a field with bson:\"_id\" tag")
+}
+
+func (r *MongoRepository[T]) ensureSimpleIndexes() error {
+	var dummy T
+	t := reflect.TypeOf(dummy)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	var indexes []mongo.IndexModel
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		fieldName := getFieldName(field)
+
+		if tag := field.Tag.Get("index"); tag != "" {
+			splitTags := strings.Split(tag, ",")
+			var indexType interface{}
+			indexOptions := options.IndexOptions{}
+			for _, splitTag := range splitTags {
+				switch splitTag {
+				case "unique":
+					indexOptions.SetUnique(true)
+				case "1", "-1":
+					indexType, _ = strconv.Atoi(splitTag)
+				case "sparse":
+					indexOptions.SetSparse(true)
+				case "text", "2dsphere":
+					indexType = splitTag
+				default:
+					return errors.New("unsupported index tag: " + splitTag)
+				}
+			}
+			index := mongo.IndexModel{
+				Keys:    bson.D{{Key: fieldName, Value: indexType}},
+				Options: &indexOptions,
+			}
+			indexes = append(indexes, index)
+		}
+	}
+	if len(indexes) > 0 {
+		_, err := r.collection.Indexes().CreateMany(context.Background(), indexes)
+		return err
+	}
+	return nil
+}
+
+func getFieldName(field reflect.StructField) string {
+	fieldName := field.Name
+	if tag := field.Tag.Get("bson"); tag != "" {
+		splitTags := strings.Split(tag, ",")
+		fieldName = splitTags[0]
+	}
+	return fieldName
+}
+
+func (r *MongoRepository[T]) ensureCompoundIndex() error {
+	var t T
+	elemType := reflect.TypeOf(t)
+	field := elemType.Field(r.idFieldIndex)
+	cindexTag := field.Tag.Get("cindex")
+	if cindexTag == "" {
+		return nil // No index to create
+	}
+
+	cleanedCindex := strings.ReplaceAll(cindexTag, "{", "")
+	cleanedCindex = strings.ReplaceAll(cleanedCindex, "}", "")
+	indexes := strings.Split(cleanedCindex, ";")
+
+	for _, index := range indexes {
+		indexKeys := bson.D{}
+		parts := strings.Split(index, ",")
+
+		for _, part := range parts {
+			kv := strings.Split(part, ":")
+			if len(kv) != 2 {
+				return fmt.Errorf("invalid compound index format: %s", part)
+			}
+
+			fieldName := kv[0]
+			order, err := strconv.Atoi(kv[1])
+			if err != nil {
+				return fmt.Errorf("invalid compound index order: %s", kv[1])
+			}
+
+			indexKeys = append(indexKeys, bson.E{Key: fieldName, Value: order})
+		}
+
+		indexModel := mongo.IndexModel{
+			Keys: indexKeys,
+		}
+
+		_, err := r.collection.Indexes().CreateOne(context.TODO(), indexModel)
+		if err != nil {
+			return fmt.Errorf("failed to create index: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func (r *MongoRepository[T]) QueryRunner() *QueryBuilder[T] {
+	return &QueryBuilder[T]{context: context.TODO(), repo: r}
+}
+
+func (r *MongoRepository[T]) FindAll() ([]T, error) {
+	var results []T
+	cursor, err := r.collection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.TODO())
+	err = cursor.All(context.TODO(), &results)
+	return results, err
+}
+
+func (r *MongoRepository[T]) FindById(id primitive.ObjectID) (T, error) {
+	var result T
+	err := r.collection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&result)
+	return result, err
+}
+
+func (r *MongoRepository[T]) FindByIds(ids []primitive.ObjectID) ([]T, error) {
+	var results []T
+	cursor, err := r.collection.Find(context.TODO(), bson.M{"_id": bson.M{"$in": ids}})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.TODO())
+	err = cursor.All(context.TODO(), &results)
+	return results, err
+}
+
+func (r *MongoRepository[T]) ExistsById(id primitive.ObjectID) (bool, error) {
+	count, err := r.collection.CountDocuments(context.TODO(), bson.M{"_id": id}, options.Count().SetLimit(1))
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
+}
+
+func (r *MongoRepository[T]) CountAll() (int64, error) {
+	count, err := r.collection.CountDocuments(context.TODO(), bson.M{})
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+func (r *MongoRepository[T]) Count(query Query) (int64, error) {
+	count, err := r.collection.CountDocuments(query.context, query.filter)
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+func (r *MongoRepository[T]) Save(item T) (T, error) {
+	v := reflect.ValueOf(&item).Elem()
+	idField := v.Field(r.idFieldIndex)
+	id := idField.Interface().(primitive.ObjectID)
+	if id.IsZero() {
+		id = primitive.NewObjectID()
+		idField.Set(reflect.ValueOf(id))
+	}
+
+	_, err := r.collection.ReplaceOne(context.TODO(), bson.M{"_id": id}, item, options.Replace().SetUpsert(true))
+	if err != nil {
+		return item, err
+	}
+	return item, nil
+}
+
+func (r *MongoRepository[T]) SaveAll(items []T) ([]T, error) {
+	var writes []mongo.WriteModel
+	for i := range items {
+		v := reflect.ValueOf(&items[i]).Elem()
+		idField := v.Field(r.idFieldIndex)
+		id := idField.Interface().(primitive.ObjectID)
+		if id.IsZero() {
+			id = primitive.NewObjectID()
+			idField.Set(reflect.ValueOf(id))
+		}
+
+		write := mongo.NewReplaceOneModel().
+			SetFilter(bson.M{"_id": id}).
+			SetReplacement(items[i]).
+			SetUpsert(true)
+		writes = append(writes, write)
+	}
+
+	_, err := r.collection.BulkWrite(context.TODO(), writes)
+	if err != nil {
+		return items, err
+	}
+	return items, nil
+}
+
+func (r *MongoRepository[T]) DeleteById(id primitive.ObjectID) error {
+	_, err := r.collection.DeleteOne(context.TODO(), bson.M{"_id": id})
+	return err
+}
+
+func (r *MongoRepository[T]) Delete(query Query) (int64, error) {
+	res, err := r.collection.DeleteMany(query.context, query.filter)
+	if err != nil {
+		return 0, err
+	}
+	return res.DeletedCount, nil
+}
+
+func (r *MongoRepository[T]) QueryOne(query Query) (T, error) {
+	var result T
+	findOptions := options.FindOne()
+	if query.projection != nil {
+		findOptions.SetProjection(query.projection)
+	}
+	dafaq := r.collection.FindOne(context.TODO(), bson.M{"name": "Query Test"})
+	err := dafaq.Decode(&result)
+	return result, err
+}
+
+func (r *MongoRepository[T]) QueryMany(query Query) ([]T, error) {
+	var results []T
+	findOptions := options.Find()
+	if query.sort != nil {
+		findOptions.SetSort(query.sort)
+	}
+	if query.projection != nil {
+		findOptions.SetProjection(query.projection)
+	}
+	if len(query.pageable) == 2 {
+		findOptions.SetSkip(int64(query.pageable[1] * query.pageable[0]))
+		findOptions.SetLimit(int64(query.pageable[1]))
+	}
+	cursor, err := r.collection.Find(query.context, query.filter, findOptions)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(query.context)
+	err = cursor.All(query.context, &results)
+	return results, err
+}
+
+func (r *MongoRepository[T]) AggregateOne(ctx context.Context, pipeline []bson.M) (bson.M, error) {
+	cursor, err := r.collection.Aggregate(ctx, pipeline)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+	var result bson.M
+	if cursor.Next(ctx) {
+		err = cursor.Decode(&result)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return result, nil
+}
+
+func (r *MongoRepository[T]) AggregateMultiple(ctx context.Context, pipeline []bson.M) ([]bson.M, error) {
+	cursor, err := r.collection.Aggregate(ctx, pipeline)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+	var results []bson.M
+	err = cursor.All(ctx, &results)
+	return results, err
+}
